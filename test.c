790318f2a5cd1b1c3162ba94b72700e4b41bfd8a
Abdelghani <abdelghani@Abdelghanis-MacBook-Air.local>
2022-06-20 03:36:23 +0100

    fix the pipe functio exp

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 23:09:35 by ababouel          #+#    #+#             */
-/*   Updated: 2022/06/19 23:54:57 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/06/20 03:29:14 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,53 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int cmdfunc(int *fdpipe, int num,int std,int pid, char **str, char **env)
+# define null   0
+# define start  1
+# define middle 2
+# define end    3
+
+int cmdfunc(int track,int *cpipe,int *pvpipe, char **str, char **env)
 {
-    pid = fork();
-    if (pid < 0)
-        return 1;
-    if (pid == 0)
+  int pid;
+  pid = fork();
+  if (pid < 0)
+    return 1;
+  if (pid == 0)
+  {
+    if (track == 1)
+      dup2(cpipe[1], STDOUT_FILENO);
+    if (track == 2)
     {
-        dup2(fdpipe[num], std);
-        close(fdpipe[0]);
-        close(fdpipe[1]);
-        execve(str[0],&str[1],env);
+      dup2(pvpipe[0], STDIN_FILENO);
+      dup2(cpipe[1], STDOUT_FILENO);
     }
-    return (pid);
+    if (track == 3)
+    {
+      dup2(cpipe[0], STDIN_FILENO);
+    }
+    execve(str[0],&str[1],env);
+  }
+  if (pid > 0)
+  {
+    if (track == 1)
+    {
+      close(cpipe[1]);
+      // return (cpipe);
+    }
+    if (track == 2)
+    {
+      close(pvpipe[0]);
+      close(cpipe[1]);
+      // return (cpipe);
+    }
+    if (track == 3)
+    {
+      close(cpipe[0]);
+      // return (cpipe);
+    }
+    waitpid(-1, NULL, 0);
+  }
+  return (0);
 }
 
 void    loop_pipe(char ***cmd) 
@@ -62,37 +96,31 @@ void    loop_pipe(char ***cmd)
     }
 }
 
-// int main(int ac, char **av, char **env)
-// {
+int main(int ac, char **av, char **env)
+{
 //     char *ls[] = {"ls", NULL};
 //     char *grep[] = {"grep", "main", NULL};
 //     char *wc[] = {"wc", NULL};
 //     char **cmd[] = {ls, grep, wc, NULL};
     
-//     loop_pipe(cmd);
-//     // int  fd1[2];
-//     // int  fd2[2];
-//     // int  pid1;
-//     // int  pid2;
-//     // int  pid3;
-//     // char *str1[100] = {"/bin/cat","cat","data"}; 
-//     // // char *str3[100] = {"/usr/bin/uniq","uniq"};
-//     // char *str2[100] = {"/usr/bin/sort","sort"};
+    // loop_pipe(cmd);
+    int  fd1[2];
+    int  fd2[2];
+    int  fd3[2];
+    char *str1[100] = {"/bin/cat","cat","data"}; 
+    char *str3[100] = {"/usr/bin/uniq","uniq"};
+    char *str2[100] = {"/usr/bin/sort","sort"};
+    char *str4[100] = {"/usr/bin/wc","wc"};
     
-//     // if (pipe(fd1) == -1)
-//     //    return (1);
-//     // // if (pipe(fd2) == -1)
-//     // //    return (2);
-//     // pid1 = cmdfunc(fd1, 1, STDOUT_FILENO, pid1, str1, env);
-//     // pid2 = cmdfunc(fd1, 0, STDIN_FILENO, pid2, str2, env);
-//     // // pid3 = cmdfunc(fd2, 0, STDIN_FILENO, pid3, str3, env);
-//     // close(fd1[0]);
-//     // close(fd1[1]);
-//     // // close(fd2[1]);
-//     // // close(fd2[0]);
-//     // waitpid(pid1, NULL, 0);
-//     // waitpid(pid2, NULL, 0);
-//     // // waitpid(pid3, NULL, 0);
-//     // printf("Im here\n");
-//     return (0); 
-// }
+    if (pipe(fd1) == -1)
+      return (1);
+    if (pipe(fd2) == -1)
+      return (2);
+    if (pipe(fd3) == -1)
+      return (3);
+    cmdfunc(start, fd1, NULL, str1, env);
+    cmdfunc(middle, fd2, fd1, str2, env);
+    cmdfunc(middle, fd3, fd2, str3, env);
+    cmdfunc(end, fd3, NULL, str4, env);
+    return (0); 
+}
