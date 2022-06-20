2eeee22b0903d46ca7e8b275231c7c24cf98abed
Abdelghani <abdelghani@Abdelghanis-MacBook-Air.local>
2022-06-20 23:51:33 +0100

    update the two function

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 23:09:35 by ababouel          #+#    #+#             */
-/*   Updated: 2022/06/20 18:28:43 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/06/20 23:49:27 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,17 +57,16 @@ int cmdfunc(int track,int *cpipe,int *pvpipe, char **str, char **env)
   return (0);
 }
 
-int cmdredirectoutput(char **str, char **env, int flag)
+int cmdredirectio(char **files,char **str, char **env, int flag, int std)
 {
   int pid;
   int file;
   int x;
-  char data[3][1000] ={"file1.txt","file2.txt","file3.txt"}; 
 
   x = 0;
-  while (x < 3)
+  while (files[x])
   {
-    file = open(data[x], O_WRONLY | O_CREAT | flag, 0777);
+    file = open(files[x], flag, 0777);
     if (file == -1)
       return (2);
     x++;
@@ -77,7 +76,7 @@ int cmdredirectoutput(char **str, char **env, int flag)
     return (1);
   if (pid == 0)
   {
-    dup2(file,STDOUT_FILENO);
+    dup2(file,std);
     close(file);
     execve(str[0],&str[1],env);
   }
@@ -86,40 +85,6 @@ int cmdredirectoutput(char **str, char **env, int flag)
     close(file);
     waitpid(-1, NULL, 0);
   }
-  close(file);
-  return (0);
-}
-
-int cmdredirectinput(char **str, char **env, int flag)
-{
-  int pid;
-  int file;
-  int x;
-  char data[3][1000] ={"file1.txt","file2.txt","file3.txt"}; 
-
-  x = 0;
-  while (x < 3)
-  {
-    file = open(data[x], flag, 0777);
-    if (file == -1)
-      return (2);
-    x++;
-  } 
-  pid = fork();
-  if (pid < 0)
-    return (1);
-  if (pid == 0)
-  {
-    dup2(file,STDIN_FILENO);
-    close(file);
-    execve(str[0],&str[1],env);
-  }
-  else
-  {
-    close(file);
-    waitpid(-1, NULL, 0);
-  }
-  close(file);
   return (0);
 }
 
@@ -134,7 +99,8 @@ int main(int ac, char **av, char **env)
     // int  fd1[2];
     // int  fd2[2];
     // int  fd3[2];
-    char *str1[100] = {"/usr/bin/wc","wc","-lwc"}; 
+    char *str1[100] = {"/bin/ls", "ls", "-la"};
+    char *str2[100] = {"file1.txt", "file3.txt", "file2.txt"}; 
     // char *str3[100] = {"/usr/bin/uniq","uniq"};
     // char *str2[100] = {"/usr/bin/sort","sort"};
     // char *str4[100] = {"/usr/bin/wc","wc"};
@@ -149,7 +115,7 @@ int main(int ac, char **av, char **env)
     // cmdfunc(middle, fd2, fd1, str2, env);
     // cmdfunc(middle, fd3, fd2, str3, env);
     // cmdfunc(end, fd3, NULL, str4, env);
-    // cmdredirectoutput(str1,env, O_APPEND);
-    cmdredirectinput(str1,env, O_RDONLY);
+    // cmdredirectoutput(str1,env, | O_APPEND);
+    cmdredirectinput(str2,str1,env, O_WRONLY | O_CREAT |O_TRUNC , STDOUT_FILENO);
     return (0); 
 }
