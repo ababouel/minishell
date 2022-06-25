f02f3f9551f8078ce7a325a9af6fa14619142193
Abdelghani Abouelmahassine <ababouel@e3r3p14.1337.ma>
2022-06-25 05:36:54 +0100

    update

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 23:09:35 by ababouel          #+#    #+#             */
-/*   Updated: 2022/06/24 06:45:53 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/06/25 05:33:54 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,36 @@
 # define start  1
 # define middle 2
 # define end    3
+typedef enum	e_type
+{	
+	TK_HEREDOC =  10,	
+	TK_RINPUT =  11,	
+	TK_DROUTPUT =  12,	
+	TK_ROUTPUT =  13	
+}	t_type;
+typedef struct file
+{
+  char    *file;
+  t_type  type;
+} t_file;
+
+typedef struct filed
+{
+  int    ffin;
+  int    ffout;
+  t_type type;
+} t_ffd;
+
+typedef struct data
+{
+  int     pfd[2];
+  t_ffd   ffd;
+  t_file  *file;
+  char    *delim;
+  char    **env;
+} t_data;
+
+
 
 int forcked()
 {
@@ -35,61 +65,99 @@ int forcked()
   return (pid);
 }
 
-
-int cmdredirectio(int *fd1,char **filein, char **fileout, char **str, char **env,int flagi, int flago)
+int filename(t_data *dt)
 {
-  int pid;
-  int fileo;
-  int filei;
-  int x;
-
+  int     x;
+  int     fd;
+  t_file  *file;
+  
   x = 0;
-  filei = -1;
-  fileo = -1;
-  // while (fileout[x])
-  // {
-  //   fileo = open(fileout[x], flago, 0777);
-  //   if (fileo == -1)
-  //     return (2);
-  //   x++;
-  // }
-  x = 0;
-  while (filein[x])
+  fd = 0;
+  file = dt->file;
+  while (file[x].file != NULL)
   {
-    filei = open(filein[x], flagi, 0777);
-    if (filei == -1)
-      return (2);
+    if (file[x].type == TK_HEREDOC)
+    {
+      close(dt->ffd.ffin);
+      dt->ffd.ffin = open("/tmp/hedoc", O_RDONLY | O_WRONLY | O_CREAT , 0777);
+      dt->ffd.type = file[x].type;
+    }
+    if (file[x].type == TK_RINPUT)
+    {
+      close(dt->ffd.ffin);
+      dt->ffd.ffin = open(file[x].file, O_RDONLY , 0777);      
+      dt->ffd.type = file[x].type;
+    }
+    if (file[x].type == TK_ROUTPUT)
+    { 
+      close(dt->ffd.ffout);
+      dt->ffd.ffout = open(file[x].file, O_RDONLY |   O_CREAT | O_TRUNC , 0777);
+      dt->ffd.type = file[x].type;
+    }
+    if (file[x].type == TK_DROUTPUT)
+    {
+      close(dt->ffd.ffout);
+      dt->ffd.ffout = open(file[x].file, O_WRONLY | O_CREAT | O_APPEND, 0777);
+      dt->ffd.type = file[x].type;
+    }
     x++;
-  }
-  pid = forcked();
-  if (pid == 0)
-  {
-    // if (filei != -1)
-    // {
-    dup2(filei, STDIN_FILENO);
-    dup2(fd1[1],STDOUT_FILENO);
-    close(filei);
-    close(fd1[1]);
-    // }
-    // if (fileo != -1)
-    // {
-    //   dup2(fileo, STDOUT_FILENO);
-    //   close(fileo);
-    // }
-   if (execve(str[0],&str[1],env) == -1)
-    perror(str[1]);
-  }
-  else
-  {
-    // if (filei != -1)
-    close(filei);
-    close(fd1[1]);
-    // if (fileo != -1)
-    //   close(fileo);
-    waitpid(-1, NULL, 0);
   }
   return (0);
 }
+
+// void cmdrdout(t_data *dt, char **cmd)
+// {
+//   dup2(dt->ffd.ffd, STDOUT_FILENO);
+//   close(dt->ffd.ffd);
+//   if (execve(cmd[0], &(cmd[1]), dt->env) == -1)
+//     perror(cmd[1]);
+// }
+
+// void cmdrdin(t_data *dt, char **cmd)
+// {
+//   dup2(dt->ffd.ffd, STDIN_FILENO);
+//   close(dt->ffd.ffd);
+//   if (execve(cmd[0], &(cmd[1]), dt->env) == -1)
+//     perror(cmd[1]);
+// }
+
+// void cmddrdin(t_data *dt, char **cmd)
+// {
+//   dup2(dt->ffd.ffd, STDIN_FILENO);
+//   close(dt->ffd.ffd);
+//   if (execve(cmd[0], &(cmd[1]), dt->env) == -1)
+//     perror(cmd[1]);
+// }
+
+// void cmddrdout(t_data *dt, char **cmd)
+// {
+//   close(dt->ffd.ffd);
+//   if (execve(cmd[0], &(cmd[1]), dt->env) == -1)
+//     perror(cmd[1]); 
+// }
+
+int redictionfunc(t_data *dt, char **cmd)
+{
+  int pid;
+
+  pid = forcked();
+  if (pid == 0)
+  {
+    filename(dt);
+    dup2(dt->ffd.ffin, STDIN_FILENO);
+    dup2(dt->ffd.ffout, STDOUT_FILENO);
+    close(dt->ffd.ffin);
+    close(dt->ffd.ffout);
+    if (execve(cmd[0], &(cmd[1]), dt->env) == -1){}
+      perror(cmd[1]); 
+  }
+  printf("hiiiiii\n"); 
+  waitpid(-1, NULL, 0);
+  close(3);
+  close(4);
+  return (pid);
+}
+
 
 int cmdfunc(int isin,int fd,char **filein,int isredirect,int track,int *cpipe,int *pvpipe, char **str, char **env)
 {
@@ -131,41 +199,35 @@ int cmdfunc(int isin,int fd,char **filein,int isredirect,int track,int *cpipe,in
   return (pid);
 }
 
-
-
+t_file  *injectfile(int x,t_file *fl,char *file, t_type type)
+{
+  fl[x].file = strdup(file);
+  fl[x].type = type;
+  return (fl);
+}
 
 int main(int ac, char **av, char **env)
-{
-//     char *ls[] = {"ls", NULL};
-//     char *grep[] = {"grep", "main", NULL};
-//     char *wc[] = {"wc", NULL};
-//     char **cmd[] = {ls, grep, wc, NULL};
-    
-    // loop_pipe(cmd);
+{ 
     int fd;
     int  fd1[2];
-    // int  fd2[2];
-    // int  fd3[2];
-    char *str1[100] = {"/bin/ls", "ls"};
-    char *str4[100] = {"/usr/bin/wc", "wc", "-c"};
-    char *str2[100] = {"file2.txt", "file3.txt", "file1.txt"}; 
-    char *str3[100] = {"file.txt"};
-    // char *str3[100] = {"/usr/bin/uniq","uniq"};
-    // char *str2[100] = {"/usr/bin/sort","sort"};
-    // char *str4[100] = {"/usr/bin/wc","wc"};
+    t_file *file;
+    t_data data;
+    char *cmd2[100] = {"/usr/bin/wc", "wc", "-c"};
+    char *cmd1[100] = {"/bin/cat", "cat"};
+    char *strout[100] = {"file2.txt", "file3.txt", "file1.txt"}; 
+    char *strin[100] = {"file.txt"};
+
+    file = malloc(sizeof(t_file) * 4);
+    file = injectfile(0, file, strout[0], TK_ROUTPUT);
+    file = injectfile(1, file, strout[1], TK_ROUTPUT);
+    file = injectfile(2, file, strout[2], TK_ROUTPUT);
+    file = injectfile(3, file, strin[0], TK_RINPUT);
     
-    if (pipe(fd1) == -1)
-      return (1);
-    // cmdredirectio(fd1,str3, NULL, str1, env, O_RDONLY , 0);
-    fd = open(str3[0],O_WRONLY|O_CREAT|O_APPEND,0777);
-    cmdfunc(1,fd ,NULL, 0, start,fd1, NULL, str1, env);
-    // cmdfunc(-1,-1 ,NULL, 0, end,fd1, NULL, str4, env);
-    // if (pipe(fd3) == -1)
-    //   return (3);
-    // cmdfunc(start, fd1, NULL, str1, env);
-    // cmdfunc(middle, fd2, fd1, str2, env);
-    // cmdfunc(middle, fd3, fd2, str3, env);
-    // cmdfunc(end, fd3, NULL, str4, env);
-    //  cmdfunc() 
+    
+    data.file = file;
+    data.env = env;
+    data.delim = "data";
+    pipe(data.pfd);
+    redictionfunc(&data, cmd1); 
     return (0); 
 }
