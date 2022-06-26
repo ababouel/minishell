5470fb40c15e85e01270d6165565c379603bcb15
Souhail Ismaili alaoui <sismaili@e3r5p13.1337.ma>
2022-06-26 22:50:04 +0100

    update

--- exec/built.c
+++ exec/built.c
@@ -6,7 +6,7 @@
 /*   By: sismaili <sismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 14:21:01 by sismaili          #+#    #+#             */
-/*   Updated: 2022/06/26 22:27:25 by sismaili         ###   ########.fr       */
+/*   Updated: 2022/06/26 22:48:42 by sismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,43 @@ void	built(t_cmd *cmd)
 	if (cmd->pathcmd)
 	{
 		if (!ft_strncmp(cmd->pathcmd, "/bin/pwd", ft_strlen(cmd->pathcmd)))
+		{
 			ft_pwd(cmd);
+			return ;
+		}
 		else if (!ft_strncmp(cmd->pathcmd, "/bin/echo", ft_strlen(cmd->pathcmd)))
+		{
 			ft_echo(cmd->cmdarg, cmd->env);
+			return ;
+		}
 		else if (!ft_strncmp(cmd->pathcmd, "/usr/bin/cd", ft_strlen(cmd->pathcmd)))
+		{
 			ft_cd(cmd);
+			return ;
+		}
 		else if (!ft_strncmp(cmd->pathcmd, "/usr/bin/env", ft_strlen(cmd->pathcmd)))
+		{
 			ft_env(cmd);
+			return ;
+		}
 	}
 	else
 	{
 		if (!ft_strncmp(cmd->cmdarg[0], "export", ft_strlen(cmd->cmdarg[0])))
 		{
 			ft_export(cmd);
+			return ;
 		}
 		else if (!ft_strncmp(cmd->cmdarg[0], "unset", ft_strlen(cmd->cmdarg[0])))
+		{
 			ft_unset(cmd);
+			return ;
+		}
 		else if (!ft_strncmp(cmd->cmdarg[0], "exit", ft_strlen(cmd->cmdarg[0])))
 		{
 			write(1, "exit\n", 6);
 			exit(0);
 		}
-		else
-			exec_cmd(cmd);
 	}
+	exec_cmd(cmd);
 }
