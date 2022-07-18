1152e7c098502ef0153c0a4ab6d8100f9e759e6b
Abdelghani Abouelmahassine <ababouel@e3r3p2.1337.ma>
2022-07-18 21:13:17 +0100

    fixed norminette folder parse

--- parse/parselst/parsepipe.c
+++ parse/parselst/parsepipe.c
@@ -12,18 +12,18 @@
 
 #include "exec.h"
 
-t_data  *parse_pipe(t_lsdata *lsdata, t_data *data)
+t_data	*parse_pipe(t_lsdata *lsdata, t_data *data)
 {
-    t_data  *new_dt;
+	t_data	*new_dt;
 
-    new_dt = NULL;
-    piped(data->pipe.pfd);
-    if (lsdata->size == 0)
-        data->pipe.statpipe = START;
-    else if (lsdata->size < lsdata->nupipe)
-        data->pipe.statpipe = UPDATE; 
-    ins_next_data(lsdata, data);
-    new_dt = init_dt(data->cmd.env);
-    data = NULL;
-    return (new_dt);
+	new_dt = NULL;
+	piped(data->pipe.pfd);
+	if (lsdata->size == 0)
+		data->pipe.statpipe = START;
+	else if (lsdata->size < lsdata->nupipe)
+		data->pipe.statpipe = UPDATE;
+	ins_next_data(lsdata, data);
+	new_dt = init_dt(data->cmd.env);
+	data = NULL;
+	return (new_dt);
 }
